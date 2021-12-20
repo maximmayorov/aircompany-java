9c102dee9bffe98f6f475f54906f0e225df85835
maximmayorov <maj-maksim@yandex.ru>
2021-12-20 21:12:40 +0300

    first commit

---
+++ Java/src/main/java/Airport.java
@@ -0,0 +1,104 @@
+import Planes.ExperimentalPlane;
+import models.MilitaryType;
+import Planes.MilitaryPlane;
+import Planes.PassengerPlane;
+import Planes.Plane;
+
+import java.util.*;
+
+public class Airport {
+    private final List<? extends Plane> planes;
+
+    public List<PassengerPlane> getPassengerPlanes() {
+        List<PassengerPlane> passengerPlanes = new ArrayList<>();
+        for (Plane plane : planes) {
+            if (plane instanceof PassengerPlane) {
+                passengerPlanes.add((PassengerPlane) plane);
+            }
+        }
+        return passengerPlanes;
+    }
+
+    public List<MilitaryPlane> getMilitaryPlanes() {
+        List<MilitaryPlane> militaryPlanes = new ArrayList<>();
+        for (Plane plane : planes) {
+            if (plane instanceof MilitaryPlane) {
+                militaryPlanes.add((MilitaryPlane) plane);
+            }
+        }
+        return militaryPlanes;
+    }
+
+    public PassengerPlane getPassengerPlaneWithMaxPassengersCapacity() {
+        List<PassengerPlane> passengerPlanes = getPassengerPlanes();
+        PassengerPlane planeWithMaxCapacity = passengerPlanes.get(0);
+        for (PassengerPlane plane : passengerPlanes) {
+            if (plane.getPassengersCapacity() > planeWithMaxCapacity.getPassengersCapacity()) {
+                planeWithMaxCapacity = plane;
+            }
+        }
+        return planeWithMaxCapacity;
+    }
+
+    public List<MilitaryPlane> getTransportMilitaryPlanes() {
+        List<MilitaryPlane> transportMilitaryPlanes = new ArrayList<>();
+        for (MilitaryPlane plane : getMilitaryPlanes()) {
+            if (plane.getMilitaryType() == MilitaryType.TRANSPORT) {
+                transportMilitaryPlanes.add(plane);
+            }
+        }
+        return transportMilitaryPlanes;
+    }
+
+    public List<MilitaryPlane> getBomberMilitaryPlanes() {
+        List<MilitaryPlane> bomberMilitaryPlanes = new ArrayList<>();
+        for (MilitaryPlane plane : getMilitaryPlanes()) {
+            if (plane.getMilitaryType() == MilitaryType.BOMBER) {
+                bomberMilitaryPlanes.add(plane);
+            }
+        }
+        return bomberMilitaryPlanes;
+
+    }
+
+    public List<ExperimentalPlane> getExperimentalPlanes() {
+        List<ExperimentalPlane> experimentalPlanes = new ArrayList<>();
+        for (Plane plane : planes) {
+            if (plane instanceof ExperimentalPlane) {
+                experimentalPlanes.add((ExperimentalPlane) plane);
+            }
+        }
+        return experimentalPlanes;
+    }
+
+    public Airport sortByMaxDistance() {
+        planes.sort(Comparator.comparingInt(Plane::getMaxFlightDistance));
+        return this;
+    }
+
+    public Airport sortByMaxSpeed() {
+        planes.sort(Comparator.comparingInt(Plane::getMaxSpeed));
+        return this;
+    }
+
+    public Airport sortByMaxLoadCapacity() {
+        planes.sort(Comparator.comparingInt(Plane::getMinLoadCapacity));
+        return this;
+    }
+
+    public List<? extends Plane> getPlanes() {
+        return planes;
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "Planes=" + planes.toString() +
+                '}';
+    }
+
+    public Airport(List<? extends Plane> planes) {
+        this.planes = planes;
+    }
+
+}
---
+++ Java/src/main/java/Planes/ExperimentalPlane.java
@@ -0,0 +1,49 @@
+package Planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalType;
+
+public class ExperimentalPlane extends Plane{
+
+    private ExperimentalType experimentalType;
+    private ClassificationLevel classificationLevel;
+
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalType experimentalType, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.experimentalType = experimentalType;
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ClassificationLevel getClassificationLevel(){
+        return classificationLevel;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel){
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ExperimentalType getExperimentalType() {
+        return experimentalType;
+    }
+
+    public void setExperimentalType(ExperimentalType experimentalType) {
+        this.experimentalType = experimentalType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "experimentalPlane{" +
+                "model='" + model + '\'' +
+                '}';
+    }
+}
